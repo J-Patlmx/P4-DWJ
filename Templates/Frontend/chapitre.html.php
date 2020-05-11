8e5a788e3d90804f15380d25c75a8e9ccba9ef0f
J-Patlmx <jp@macbook-pro-de-jp.home>
2020-05-11 19:01:33 +0200

    un peu de factorisation de mon css modification dune partie html de chapitre.html.php   toujour le soucis du decalage une fois le login fait  a revoir rapidement

--- Templates/Frontend/chapitre.html.php
+++ Templates/Frontend/chapitre.html.php
@@ -1,49 +1,47 @@
 <h1>Jean Forteroche !</h1>
-        <p><a href="index.php">Retour à l'accueil</a></p>
+<p><a href="index.php">Retour à l'accueil</a></p>
 
 
-        <article class="aperçu_chapitre">
-           <h2>
-               <?= $chapitre->titre;?>
-           </h2>
-           <span>
-           <?= $chapitre->date_creation;?>
-           </span> 
-           <div>
-               <p>
-               <?= $chapitre->contenu;?>
-               </p>
-           </div>
-        </article>
+<article class="aperçu_chapitre">
+    <h2>
+        <?= $chapitre->titre; ?>
+    </h2>
+    <span>
+        <?= $chapitre->date_creation; ?>
+    </span>
+    <div>
+        <p>
+            <?= $chapitre->contenu; ?>
+        </p>
+    </div>
+</article>
 
 
-        <h2 id="commentairetitre">Commentaires</h2>
-        <form class="form_commentaire" id="newcomment" action="index.php?action=addCommentaire&id=<?= $chapitre->id?>" method="post">
-      <div>
-          <label for="pseudo">Auteur</label>
-          <input type="text" id="pseudo" name="pseudo" />
-      </div>
-      <div>
-          <label for="contenu_commentaire">commentaire</label>
-          <textarea id="contenu_commentaire" name="contenu_commentaire"></textarea>
-      </div>
-      <div>
-          <input type="submit" />
-      </div>
-  </form>
+<h2 id="commentairetitre">Commentaires</h2>
+<form class="form_commentaire" id="newcomment" action="index.php?action=addCommentaire&id=<?= $chapitre->id ?>" method="post">
+    <div>
+        <label for="pseudo">Auteur</label>
+        <input type="text" id="pseudo" name="pseudo" />
+    </div>
+    <div>
+        <label for="contenu_commentaire">commentaire</label>
+        <textarea id="contenu_commentaire" name="contenu_commentaire"></textarea>
+    </div>
+    <div>
+        <input type="submit" />
+    </div>
+</form>
+
+<div id="commentairesurchapitre">
         <?php
-        foreach ($commentaires AS $commentaire) {
+        foreach ($commentaires as $commentaire) {
             ?>
-            <div class="com">
-                <p><strong><?php echo htmlspecialchars($commentaire['pseudo']); ?></strong><strong id="datecom"> le <?php echo $commentaire['date_commentaire']; ?></strong></p>
-                <p><?php echo nl2br(htmlspecialchars($commentaire['contenu_commentaire'])); ?></p>
-                <button>
-                    <a href="index.php?action=signalerUnCommentaire&id=<?= $commentaire['id'] ?>"><i class="fas fa-thumbs-down"></i></a>
-                </button>
-
-            </div>
+           <div class="element"><strong><?php echo htmlspecialchars($commentaire['pseudo']); ?></strong><strong id="datestyle"> le <?php echo $commentaire['date_commentaire']; ?></strong></div>
+                  <div class="element"><?php echo nl2br(htmlspecialchars($commentaire['contenu_commentaire'])); ?></div>
+                    <button>
+                     <a class="element" href="index.php?action=signalerUnCommentaire&id=<?= $commentaire['id'] ?>"><i class="fas fa-thumbs-down"></i></a>
+                    </button> <br />
         <?php
         } // Fin de la boucle des commentaires
-        ?>
-  
-        
+         ?> 
+    </div> <br />
